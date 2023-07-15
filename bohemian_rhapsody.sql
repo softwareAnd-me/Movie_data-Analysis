9c68959412a67dbf1c1c0ee0c25dda824a21c00b
Genesis0505 <kirlinmensah@gmail.com>
2023-07-15 01:01:20 +0000

    Wordlist with words and frequency for Bohemian Rhapsody

---
+++ bohemian_rhapsody.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE MovieCloudList;
+USE MovieCLoudList;
+CREATE TABLE Bohemian_Rhapsody(Frequency INT(255), Word VARCHAR(255));
+CREATE TABLE La_La_Land(Frequency INT(255), Word VARCHAR(255));
+SHOW TABLES;
+
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(12, "band");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(7, "live");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(7, "aid");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(7, "gay");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(6, "performance");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(5, "hits");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(5, "awards");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(5, "time");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(5, "writing");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "lead");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "rock");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "relationship");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "music");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "average");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "score");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(4, "gave");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "attracted");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "learns");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "singer");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "album");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "however");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "close");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "solo");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "best");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "job");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "critic");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "truth");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "spectator");
+INSERT INTO Bohemian_Rhapsody(Frequency, Word)
+VALUES(3, "life");
+
+SELECT *
+FROM Bohemian_Rhapsody;
